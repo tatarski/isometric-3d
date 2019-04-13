a2cb21d598875f3736b3104877dd11e4c9acd5c8
tatarski <tatarski.dim@gmail.com>
2019-04-14 01:25:25 +0300

    Player, player movement

--- field.js
+++ field.js
@@ -1,9 +1,23 @@
-function drawField(field, sideLength, cameraX, cameraY) {
+function drawField(field,
+                   sideLength,
+                   cameraX,
+                   cameraY,
+                   playerFloor,
+                   playerRow,
+                   playerColumn) {
   for(var floor = 0; floor < field.floors; floor++) {
     for(var row = 0; row < field.rows; row++) {
       for(var column = 0; column < field.columns; column++) {
-        if(field[floor][row][column] === 1) {
-          drawCube(
+        if(floor === playerFloor &&
+          row === playerRow &&
+          column === playerColumn) {
+          defaultFillStyle = "red";
+        } else if(field[floor][row][column] === 1) {
+          defaultFillStyle = "yellow";
+        } else {
+          continue
+        }
+        drawCube(
             cameraX + fromFieldToScreenX(
             floor, row, column, sideLength
             ),
@@ -12,12 +26,11 @@ function drawField(field, sideLength, cameraX, cameraY) {
             ),
             sideLength
           );
-        }
       }
     }
   }
 }
-function getField(floors, rows, columns) {
+function getField(floors, rows, columns, valueForCoordinates) {
   var field = [];
   field.floors = floors;
   field.rows = rows;
@@ -28,7 +41,7 @@ function getField(floors, rows, columns) {
     for(var row = 0; row < rows; row++) {
       field[floor][row] = [];
       for(var column = 0; column < columns; column++) {
-        field[floor][row][column] = Math.floor(Math.random()*2);
+        field[floor][row][column] = valueForCoordinates(floor, row, column);
       }
     }
   }
--- game.js
+++ game.js
@@ -1,11 +1,49 @@
-var field = getField(3, 10, 10);
+var field = getField(5, 10, 10, function valueForCoordinates(floor, row, column) {
+  if(floor < 3) {
+    return 1;
+  }else {
+    return 0;
+  }
+});
+field[3][4][4] = 1;
 var side_length = 50;
+var playerFloor = 3, playerRow = Math.floor(field.rows/2), playerColumn = Math.floor(field.columns/2);
+
 function update() {
 }
 function draw() {
-  drawField(field, side_length, 300, 300);
+  drawField(field, side_length, 300, 300, playerFloor, playerRow, playerColumn);
+}
+function canChangeColumn(columnDelta, playerFloor, playerRow, playerColumn, field) {
+  return playerColumn + columnDelta >= 0 && playerColumn + columnDelta < field.columns;
+}
+function canChangeRow(rowDelta, playerFloor, playerRow, playerColumn, field) {
+  return playerRow + rowDelta >= 0 && playerRow + rowDelta < field.rows;
 }
 function keyup(key) {
+  var columnDelta = 0, rowDelta = 0;
+  // W
+  if(key == 87) {
+    rowDelta = -1
+  }
+  // S
+  if(key == 83) {
+    rowDelta = +1;
+  }
+  // A
+  if(key == 65) {
+    columnDelta = -1;
+  }
+  // D
+  if(key == 68) {
+    columnDelta = +1;
+  }
+  if(columnDelta && canChangeColumn(columnDelta, playerFloor, playerRow, playerColumn, field)) {
+    playerColumn += columnDelta;
+  }
+  if(rowDelta && canChangeRow(rowDelta, playerFloor, playerRow, playerColumn, field)) {
+    playerRow += rowDelta;
+  }
 }
 function mouseup() {
 }
