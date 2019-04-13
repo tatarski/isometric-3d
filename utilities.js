88a7ae4938fbd9237bf4a0a3e948c186b0c5c037
tatarski <tatarski.dim@gmail.com>
2019-04-14 01:01:44 +0300

    Fix indentation

--- utilities.js
+++ utilities.js
@@ -1,6 +1,6 @@
 function fromFieldToScreenX(floor, row, column, sideLength) {
-    return 0*floor - row*sideLength/2 + column*sideLength
+  return 0*floor - row*sideLength/2 + column*sideLength
 }
 function fromFieldToScreenY(floor, row, column, sideLength) {
-    return -floor*sideLength + row*sideLength/2 + 0*sideLength;
+  return -floor*sideLength + row*sideLength/2 + 0*sideLength;
 }
